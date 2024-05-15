fb370877ee4886503a2d5e78374515e65ba04e9e
kirti7464 <71482336+kirti7464@users.noreply.github.com>
2024-05-15 23:45:54 +0530

    Add files via upload

---
+++ Notes/JS/syntax.js
@@ -0,0 +1,3 @@
+// JavaScript is a dynamic programming language, which means do not need to specify the type of variable. 
+// There are two types of data types in JavaScript - Primitive, non-Primitive
+
